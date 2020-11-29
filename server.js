e7cd857d359c02b7d97be32adcd5fffaa7009816
Xingjian <xingjian@pdx.edu>
2020-11-28 22:50:47 -0800

    Added the function of adding room, to be tested.

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ io.on('connection', (socket) => {
   console.log('client connected');
 
   let elems = [];
+  var rooms=[];
+  var room_people=[];
 
   // elems.forEach((item) => {
   //   io.emit('clk', item);
@@ -34,6 +36,29 @@ io.on('connection', (socket) => {
     elems[data.pi*100+data.pj] = data;
 
     io.emit('clk', data);
+  });
+
+  socket.on('joinRoom',(data) =>{
+    var join=false;
+    if(rooms.indexOf(data.roomNum)==-1){
+      rooms.push(data.roomNum);
+      room_people.push(1);
+      join=true;
+    }
+    else{
+      var index=rooms.indexOf(data.roomNum);
+      if(room_people[index]==1){
+        room_people[index]=2;
+        join=true;
+      }
+      else{
+        join=false;
+      }
+    }
+    io.emit('assignRoom',{
+      join: join,
+      room_people: room_people[rooms.indexOf(data.roomNum)],
+    });
   });
 
   socket.on('disconnect', () => {
